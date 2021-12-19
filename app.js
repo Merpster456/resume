6cb9465b0e313223b78ba2b39654a51fd4a8b846
Merpster <ryan.quirk6@protonmail.ch>
2021-12-18 20:45:33 -0500

    Adding materialize design elements

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.render('index');
     console.log('Viewer landed!')
 })
-
+app.use(express.static("public"));
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("Website Launched");
